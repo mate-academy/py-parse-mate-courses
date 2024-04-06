e9eac5198518ba1b894c7f45484c14a527a8394b
maxitoska <maxitosm00@gmail.com>
2024-04-06 21:32:40 +0300

    fixes

--- app/parse.py
+++ app/parse.py
@@ -23,34 +23,59 @@ class Course:
 
 
 def get_all_courses() -> list[Course]:
+    all_courses = []
     page = requests.get(MATE_URL).content
     soup = BeautifulSoup(page, "html.parser")
     courses = soup.select(".ProfessionCard_cardWrapper__JQBNJ")
-    return [parse_single_course(course) for course in courses]
+    for course in courses:
+        all_courses.extend(parse_single_course(course))
+    return all_courses
 
 
-def parse_single_course(course_soup: BeautifulSoup) -> Course:
+def parse_single_course(
+        course_soup: BeautifulSoup
+) -> tuple[Course] | tuple[Course, Course]:
     if "data-qa" in course_soup.attrs:
-        course_type = get_course_type(course_soup)
+        course_types = get_course_type(course_soup)
         modules, topics, duration = get_moduls_topics_and_duration(course_soup)
-        return Course(
-            name=course_soup.select_one("a").text,
-            short_description=course_soup.select_one(".mb-32").text,
-            course_type=course_type,
-            modules=modules,
-            topics=topics,
-            duration=duration
-        )
+        if len(course_types) == 1:
+            return (Course(
+                name=course_soup.select_one("a").text,
+                short_description=course_soup.select_one(".mb-32").text,
+                course_type=course_types[0],
+                modules=modules,
+                topics=topics,
+                duration=duration
+            ),)
+        else:
+            return (Course(
+                name=course_soup.select_one("a").text,
+                short_description=course_soup.select_one(".mb-32").text,
+                course_type=course_types[0],
+                modules=modules,
+                topics=topics,
+                duration=duration
+            ), Course(
+                name=course_soup.select_one("a").text,
+                short_description=course_soup.select_one(".mb-32").text,
+                course_type=course_types[1],
+                modules=modules,
+                topics=topics,
+                duration=duration
+            ))
+
     else:
         pass
 
 
-def get_course_type(course_soup: BeautifulSoup) -> CourseType:
+def get_course_type(
+        course_soup: BeautifulSoup
+) -> list[CourseType] | list[CourseType, CourseType]:
     course_types = course_soup.select(".ProfessionCard_buttons__a0o60 > a")
     if len(course_types) == 2:
-        return CourseType.FULL_TIME
+        return [CourseType.FULL_TIME, CourseType.PART_TIME]  # wrong
     else:
-        return CourseType.PART_TIME
+        return [CourseType.PART_TIME]
 
 
 def get_moduls_topics_and_duration(course_soup: BeautifulSoup) -> tuple:
