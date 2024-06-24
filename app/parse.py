3ea22d2cb69c00bfc06546fb009386f6e31a18db
Oleksandra <aleksandra@regdesk.co>
2024-06-24 13:29:47 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,10 @@
+import requests
+
 from dataclasses import dataclass
 from enum import Enum
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/"
 
 
 class CourseType(Enum):
@@ -14,5 +19,39 @@ class Course:
     course_type: CourseType
 
 
+def parse_course(course_soup: BeautifulSoup) -> list[Course]:
+    name = course_soup.select_one("a[class*='ProfessionCard_title'] > h3").text
+    short_description = course_soup.select_one("p:not([class*='ProfessionCard_subtitle'])").text
+    fulltime = course_soup.select_one("a[data-qa=fulltime-course-more-details-button]")
+    parttime = course_soup.select_one("a[data-qa=parttime-course-more-details-button]")
+
+    result = []
+
+    if fulltime:
+        result.append(Course(
+            name=name,
+            short_description=short_description,
+            course_type=CourseType.FULL_TIME
+        ))
+    if parttime:
+        result.append(Course(
+            name=name,
+            short_description=short_description,
+            course_type=CourseType.PART_TIME
+        ))
+
+    return result
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    courses = BeautifulSoup(
+        requests.get(BASE_URL).content,
+        "html.parser"
+    ).select("div[class*='ProfessionCard_cardWrapper']")
+
+    result = []
+
+    for course in courses:
+        result.extend(parse_course(course))
+
+    return result
