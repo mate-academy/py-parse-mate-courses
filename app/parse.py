2ecc1ea168697a144ba6e2b6d367aeedc7aad51a
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-06-28 22:54:05 +0200

    made solution with extra task: added count of modules, topics, duration

--- app/parse.py
+++ app/parse.py
@@ -17,36 +17,83 @@ class Course:
     name: str
     short_description: str
     course_type: CourseType
+    modules: int
+    topics: int
+    duration: int
 
 
-def parse_single_course(soup: BeautifulSoup, course_type: CourseType) -> Course:
+def fetch_html(url: str) -> BeautifulSoup:
+    response = requests.get(url)
+    return BeautifulSoup(response.content, "html.parser")
+
+
+def parse_single_course(
+        course_card: BeautifulSoup,
+        course_page: BeautifulSoup,
+        course_type: CourseType
+) -> Course:
     return Course(
-        name=soup.select_one("a").text.strip(),
-        short_description=soup.select_one("p.typography_landingTextMain__Rc8BD.mb-32").text.strip(),
-        course_type=course_type
+        name=course_card.select_one("a").text,
+        short_description=course_card.select_one(
+            "p.typography_landingTextMain__Rc8BD.mb-32"
+        ).text,
+        course_type=course_type,
+        modules=int(
+            course_page.select_one(
+                ".CourseModulesHeading_modulesNumber__UrnUh > p"
+            ).text.split()[0]
+        ),
+        topics=int(
+            course_page.select_one(
+                ".CourseModulesHeading_topicsNumber__5IA8Z > p"
+            ).text.split()[0]
+        ),
+        duration=int(
+            course_page.select_one(
+                ".CourseModulesHeading_courseDuration__qu2Lx > p"
+            ).text.split()[0]
+        ),
     )
 
 
-def get_courses(course_type: CourseType, data_qa_selector: str) -> list[Course]:
+def get_courses(
+        course_type: CourseType,
+        data_qa_selector: str
+) -> list[Course]:
     all_courses = []
 
-    response = requests.get(BASE_URL)
-    soup = BeautifulSoup(response.content, "html.parser")
+    soup = fetch_html(BASE_URL)
     courses = soup.select(".ProfessionCard_cardWrapper__JQBNJ")
 
     for course in courses:
-        if course.find('a', {'data-qa': data_qa_selector}):
-            all_courses.append(parse_single_course(course, course_type))
+        link = course.find("a", {"data-qa": data_qa_selector})
+        if link:
+            course_page_url = BASE_URL + link["href"]
+            course_page_soup = fetch_html(course_page_url)
+            course_detail_sections = course_page_soup.select(
+                ".CourseModulesHeading_headingGrid__ynoxV"
+            )
+            for course_detail_section in course_detail_sections:
+                all_courses.append(
+                    parse_single_course(
+                        course,
+                        course_detail_section,
+                        course_type
+                    )
+                )
 
     return all_courses
 
 
 def get_full_time_courses() -> list[Course]:
-    return get_courses(CourseType.FULL_TIME, 'fulltime-course-more-details-button')
+    return get_courses(
+        CourseType.FULL_TIME,
+        "fulltime-course-more-details-button"
+    )
 
 
 def get_part_time_courses() -> list[Course]:
-    return get_courses(CourseType.PART_TIME, 'fx-course-details-button')
+    return get_courses(CourseType.PART_TIME, "fx-course-details-button")
 
 
 def get_all_courses() -> list[Course]:
