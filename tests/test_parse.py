c7aac9db13870c1ad8228d3edb6b021a78e1724f
Serhii-Leonenko <leonenko3149@gmail.com>
2024-08-27 17:40:31 +0300

    Update test_parse.py

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,22 +1,22 @@
-from app.parse import get_all_courses, CourseType
-
+from app.parse import get_all_courses
 
 FOR_SURE_THIS_COURSES = [
     "QA",
     "Java",
     "Python",
+    "Recruiter",
+    "DevOps",
+    "Digital",
+    "Data"
 ]  # frontend is web development sometimes
 
 
 def test_get_all_courses():
     all_courses = get_all_courses()
 
-    for course_type in CourseType:
-        course_names = [
-            course.name for course in all_courses if course.course_type == course_type
-        ]
+    course_names = [course.name for course in all_courses]
 
-        for course in FOR_SURE_THIS_COURSES:
-            assert any(
-                course.lower() in course_name.lower() for course_name in course_names
-            ), f"Course '{course}' have not been parsed for '{course_type}'"
+    for course in FOR_SURE_THIS_COURSES:
+        assert any(
+            course.lower() in course_name.lower() for course_name in course_names
+        ), f"Course '{course}' have not been parsed"
