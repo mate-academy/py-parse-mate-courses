494379ff204039d0f3a70436a5f31c812e0ce2e1
AndrewwSSS <clash2clans1one@gmail.com>
2024-08-28 13:10:50 +0300

    code reformat according pep8

--- app/parse.py
+++ app/parse.py
@@ -19,10 +19,14 @@ class Course:
     count_topics: int
 
 
-async def get_detail_course(url: str, client: httpx.AsyncClient) -> dict[str, int]:
+async def get_detail_course(
+        url: str, client: httpx.AsyncClient
+) -> dict[str, int]:
     response = await client.get(f"{BASE_URL}{url}")
     soup = BeautifulSoup(response.text, "html.parser")
-    data_container = soup.select_one("#course-program .CourseModulesHeading_headingGrid__ynoxV")
+    data_container = soup.select_one(
+        "#course-program .CourseModulesHeading_headingGrid__ynoxV"
+    )
     return {
         "count_modules": int(data_container.select_one(
             ".CourseModulesHeading_modulesNumber__UrnUh > p"
@@ -39,7 +43,9 @@ async def parse_course(tag: bs4.Tag, client: httpx.AsyncClient) -> Course:
     )
     return Course(
         name=tag.select_one("a.typography_landingH3__vTjok > h3").text,
-        short_description=tag.select("p.typography_landingTextMain__Rc8BD")[1].text,
+        short_description=tag.select(
+            "p.typography_landingTextMain__Rc8BD"
+        )[1].text,
         duration=tag.select(".ProfessionCard_subtitle__K1Yp6 > span")[0].text,
         count_modules=extra_data["count_modules"],
         count_topics=extra_data["count_topics"],
@@ -62,11 +68,15 @@ async def get_all_courses() -> list[Course]:
 
 def write_courses_json(courses: list[Course], file_name: str) -> None:
     with open(file_name, "w", encoding="utf-8") as f:
-        data = json.dumps([asdict(course) for course in courses], indent=4, ensure_ascii=False)
+        data = json.dumps(
+            [asdict(course) for course in courses],
+            indent=4,
+            ensure_ascii=False
+        )
         f.write(data)
 
 
-async def main():
+async def main() -> None:
     write_courses_json(await get_all_courses(), "data.json")
 
 
