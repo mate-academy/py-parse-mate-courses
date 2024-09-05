79609919622340a07e4ecd5aff0087d4bf668b52
Oleksandr Pastukhov <lotrikys@gmail.com>
2024-09-05 15:43:13 +0300

    * Make main task

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 
+import requests
+from bs4 import BeautifulSoup, Tag
+
+BASE_URL = "https://mate.academy"
+
 
 @dataclass
 class Course:
@@ -8,5 +13,27 @@ class Course:
     duration: str
 
 
+def parse_course(soup: Tag) -> Course:
+    return Course(
+        name=soup.select_one("a > h3").text,
+        short_description=soup.select_one("p:nth-child(3)").text,
+        duration=soup.select_one("p > span").text,
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    response = requests.get(f"{BASE_URL}/").content
+    courses_soup = BeautifulSoup(response, "html.parser").select(
+        "[data-qa='profession-card']"
+    )
+
+    courses = []
+
+    for course_soup in courses_soup:
+        courses.append(parse_course(course_soup))
+
+    return courses
+
+
+if __name__ == "__main__":
+    get_all_courses()
