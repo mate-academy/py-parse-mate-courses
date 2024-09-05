428bfc0aec391c9e36d0a9cadca1f79e012377b2
Oleksandr Pastukhov <lotrikys@gmail.com>
2024-09-05 16:06:56 +0300

    * Add new fields for modules count and topics count

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup, Tag
@@ -11,13 +12,31 @@ class Course:
     name: str
     short_description: str
     duration: str
+    modules_count: int
+    topics_count: int
 
 
 def parse_course(soup: Tag) -> Course:
+    detail_url = urljoin(
+        BASE_URL,
+        soup.select_one("a")["href"],
+    )
+
+    detail_course = requests.get(detail_url).content
+    detail_soup = BeautifulSoup(detail_course, "html.parser")
+    modules_count = detail_soup.select("[class*='CourseModulesHeading_text']")[
+        0
+    ].text.split()[0]
+    topics_count = detail_soup.select("[class*='CourseModulesHeading_text']")[
+        1
+    ].text.split()[0]
+
     return Course(
         name=soup.select_one("a > h3").text,
         short_description=soup.select_one("p:nth-child(3)").text,
         duration=soup.select_one("p > span").text,
+        modules_count=int(modules_count),
+        topics_count=int(topics_count),
     )
 
 
