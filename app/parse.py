727ec9c0928668a34cd5f43c4294213015386eb8
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-11-03 21:03:18 +0200

    Fix flake8 tests

--- app/parse.py
+++ app/parse.py
@@ -16,7 +16,7 @@ class Course:
     duration: str
 
 
-def get_course_info_soup(course_soup: BeautifulSoup):
+def get_course_info_soup(course_soup: BeautifulSoup) -> BeautifulSoup:
     course_info_page = course_soup.select_one("a")["href"]
     course_url = urljoin(BASE_URL, course_info_page)
 
@@ -31,18 +31,22 @@ def parse_course_name(course_soup: BeautifulSoup) -> Course:
     return Course(
         name=course_soup.select_one("a")["title"],
         short_description=get_course_info_soup(course_soup).find(
-            class_="typography_landingTextMain__Rc8BD SalarySection_aboutProfession__1VFHK"
+            class_="typography_landingTextMain__Rc8BD"
+                   " SalarySection_aboutProfession__1VFHK"
         ).text,
         duration=get_course_info_soup(course_soup).find_all(
-            class_="ComparisonTable_cell__RNsyU ComparisonTable_highlightCell__IpyWZ typography_newLandingH6__Av9tT"
+            class_="ComparisonTable_cell__RNsyU ComparisonTable_highl"
+                   "ightCell__IpyWZ typography_newLandingH6__Av9tT"
         )[12].text
     )
 
 
-def get_all_courses():
+def get_all_courses() -> list[Course]:
     page = requests.get(BASE_URL).content
     base_page_soup = BeautifulSoup(page, "html.parser")
-    courses = base_page_soup.select(".HeaderCoursesDropdown_coursesListItem__36tl_")
+    courses = base_page_soup.select(
+        ".HeaderCoursesDropdown_coursesListItem__36tl_"
+    )
     return [parse_course_name(course_soup) for course_soup in courses]
 
 
