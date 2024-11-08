8e0298ed0df4ef8f610013320e6a77e23f2e2c2b
Alex Shcherbakov <oleksa.shcherbakov@gmail.com>
2024-11-08 13:39:00 +0200

    'Solution'

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
+
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/"
+OUTPUT_CSV_PATH = "course.csv"
 
 
 @dataclass
@@ -8,5 +15,40 @@ class Course:
     duration: str
 
 
-def get_all_courses() -> list[Course]:
-    pass
+COURSE_FIELDS = [field.name for field in fields(Course)]
+
+
+def parse_single_course(page_soup: BeautifulSoup) -> Course:
+    return Course(name=page_soup.select_one("h3").text,
+                  short_description=page_soup.select_one(
+                      ".typography_landingTextMain__Rc8BD.mb-32"
+                  ).text,
+                  duration=page_soup.select_one(
+                      "p.typography_landingTextMain__Rc8BD.ProfessionCardTags_regularTag__yTc6K:last-of-type > span:last-of-type"
+                  ).text
+                  )
+
+
+def get_all_courses() -> [Course]:
+    page = requests.get(BASE_URL).content
+    page_soup = BeautifulSoup(page, "html.parser")
+    courses = page_soup.select(".ProfessionCard_cardWrapper__JQBNJ")
+
+    return [parse_single_course(course_soup) for course_soup in courses]
+
+
+def write_products_to_csv(courses: [Course]) -> None:
+    with open(OUTPUT_CSV_PATH, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(COURSE_FIELDS)
+        writer.writerows([astuple(course) for course in courses])
+
+
+def main():
+    courses = get_all_courses()
+    print(courses)
+    write_products_to_csv(courses)
+
+
+if __name__ == "__main__":
+    main()
