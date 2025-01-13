b9aecbc814f42d54820e61ccc6467c3ba5e4c233
Max <mxbely98@gmail.com>
2025-01-13 09:28:25 +0200

    implement parsing mate home page

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,9 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
+import requests
+from bs4 import BeautifulSoup
+
+
+HOME_URL = "https://mate.academy/"
 
 
 @dataclass
@@ -8,5 +13,28 @@ class Course:
     duration: str
 
 
+QUOTE_FIELDS = [field.name for field in fields(Course)]
+
+
+def parse_single_quote(course: BeautifulSoup) -> Course:
+    return Course(
+        name=course.select_one("h3").text,
+        short_description=course.select_one(
+            ".typography_landingTextMain__Rc8BD.mb-32"
+        ).text,
+        duration=course.select_one(
+            ".ProfessionCardTags_regularTag__dqOGj:last-child span"
+        ).text,
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    text = requests.get(HOME_URL).content
+    soup = BeautifulSoup(text, "html.parser")
+    courses = soup.select(".ProfessionCard_cardWrapper__2Q8_V")
+    recived_course = [parse_single_quote(course) for course in courses]
+    return recived_course
+
+
+if __name__ == "__main__":
+    get_all_courses()
