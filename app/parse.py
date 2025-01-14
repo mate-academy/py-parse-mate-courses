ed0eaf86807daa13f29e42d31ca6c9916b578a2e
RuslanTopal <baranotik15@gmail.com>
2025-01-14 09:15:51 +0000

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,10 @@
-from dataclasses import dataclass
+import requests
+import csv
+from bs4 import BeautifulSoup, Tag
+from dataclasses import dataclass, fields, astuple
+
+
+BASE_URL = "https://mate.academy/"
 
 
 @dataclass
@@ -8,5 +14,57 @@ class Course:
     duration: str
 
 
+COURSE_FIELDS = [field.name for field in fields(Course)]
+
+
+def parse_single_course(course: Tag) -> Course:
+    name = course.select_one("a.typography_landingH3__vTjok h3")
+    short_description = (
+        course.select_one("p.typography_landingTextMain__Rc8BD.mb-32"))
+    duration_tags = (course.select(
+        ".ProfessionCard_professionTags__2iarD.mb-24 span")
+    )
+
+    if not all([name, short_description, duration_tags]):
+        raise ValueError("Missing required course data")
+
+    last_duration = duration_tags[-1].text.strip() if duration_tags else ""
+
+    return Course(
+        name=name.text.strip(),
+        short_description=short_description.text.strip(),
+        duration=last_duration
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    response = requests.get(BASE_URL)
+    soup = BeautifulSoup(response.text, "html.parser")
+    course_elements = soup.select("div.ProfessionCard_cardWrapper__2Q8_V")
+    courses = []
+
+    for element in course_elements:
+        try:
+            course = parse_single_course(element)
+            courses.append(course)
+        except AttributeError:
+            continue
+
+    return courses
+
+
+def write_courses_to_csv(courses: [Course], path: str) -> None:
+    """Save courses to CSV file"""
+    with open(path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(COURSE_FIELDS)
+        writer.writerows([astuple(course) for course in courses])
+
+
+def main(output_csv_path: str) -> None:
+    courses = get_all_courses()
+    write_courses_to_csv(courses, output_csv_path)
+
+
+if __name__ == "__main__":
+    main("courses.csv")
