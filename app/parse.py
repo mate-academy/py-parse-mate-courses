9f49d8b0474c5df9bb899ed64ccec5ef4b055d19
Anton Konyk <antonkonyk.com>
2025-01-15 16:36:36 +0100

    create parse_single_course

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from bs4 import BeautifulSoup, Tag
 
 
 @dataclass
@@ -6,6 +7,18 @@ class Course:
     name: str
     short_description: str
     duration: str
+
+
+def parse_single_course(course: Tag) -> Course:
+
+    return Course(
+        name=course.select_one("a.typography_landingH3__vTjok h3").text,
+        short_description=course.find(
+            "p", class_="typography_landingTextMain__Rc8BD mb-32").get_text(),
+        duration=course.select(
+            "p.typography_landingTextMain__Rc8BD.ProfessionCardTags_regularTag__dqOGj")[-1].
+        find("span").text,
+    )
 
 
 def get_all_courses() -> list[Course]:
