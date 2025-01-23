c0af2ec9892d2ecea97a381b92374218ec714b10
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2025-01-23 17:18:29 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 
+import requests
+from bs4 import BeautifulSoup, Tag
+
+BASE_URL = "https://mate.academy/"
+
 
 @dataclass
 class Course:
@@ -8,5 +13,20 @@ class Course:
     duration: str
 
 
+def parse_single_course(course: Tag) -> Course:
+    return Course(
+        name=course.select_one(".ProfessionCard_title__fEWio").text,
+        short_description=course.select_one(
+            ".typography_landingTextMain__Rc8BD.mb-32"
+        ).text,
+        duration=course.select_one(
+            ".ProfessionCardTags_regularTag__dqOGj"
+        ).text,
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    text = requests.get(BASE_URL).content
+    soup = BeautifulSoup(text, "html.parser")
+    courses = soup.select(".ProfessionCard_cardWrapper__2Q8_V")
+    return [parse_single_course(course) for course in courses]
