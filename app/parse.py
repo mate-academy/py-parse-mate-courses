8fba84e777b0a9382b712e90b514ba05771224e7
lev2099 <lev2099@gmail.com>
2025-02-15 22:39:39 +0200

    Add course parsing functionality with BeautifulSoup
    This update introduces a web scraping feature to fetch courses from a predefined URL. It uses BeautifulSoup to parse HTML and extract course details into dataclass objects. The function `get_all_courses` now returns a list of parsed courses.

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/#all-courses"
 
 @dataclass
 class Course:
@@ -8,5 +12,19 @@ class Course:
     duration: str
 
 
+def parse_course(course):
+    return Course(
+        name=course.select_one(".ProfessionCard_title__m7uno").text,
+        short_description=course.select_one(".ProfessionCard_description__K8weo").text,
+        duration=course.select_one(".ProfessionCard_duration__13PwX").text
+
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    page_html = requests.get(BASE_URL).text
+    soup_page = BeautifulSoup(page_html, "html.parser")
+    courses = soup_page.select(".ProfessionsListSectionTemplate_card__ZNsgf")
+    return [parse_course(course) for course in courses]
+
+print(get_all_courses())
